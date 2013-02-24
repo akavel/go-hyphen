0d83d05d76241314f6637b19d6d128139f6be377
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2013-02-24 01:38:07 +0100

    epub-hyph: test hyphenation in HTML <body>

--- hyphenate/epub-hyph/epub-hyph.go
+++ hyphenate/epub-hyph/epub-hyph.go
@@ -2,13 +2,98 @@ package main
 
 import (
 	"archive/zip"
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
 	"github.com/akavel/go-epub/hyphenate"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"unicode"
 )
+
+const (
+	htmlBase = iota
+	htmlTag
+	htmlBody
+	htmlTagInBody
+)
+
+var pattBody = regexp.MustCompile(`^\s*([^:]*:\s*)?\b[Bb][Oo][Dd][Yy]\b`) // start simple
+var utf8softhyphen = []byte{0xc2, 0xad}
+
+func hyphHtml(r io.Reader, w io.Writer, h *hyphenate.Hyphenations) error {
+	// TODO: skip HTML comments
+	// TODO: skip SVG etc.; maybe handle only selected elems?... but html5 maybe allows variety?
+	// TODO: in case of problems, try using code.google.com/p/go.net/html parser
+	br := bufio.NewReader(r)
+	bw := bufio.NewWriter(w)
+	word := make([]rune, 0, 128)
+	state := htmlBase
+	for {
+		c, sz, err := br.ReadRune()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if c == unicode.ReplacementChar && sz == 1 {
+			return errors.New("Invalid UTF-8 code in input")
+		}
+
+		switch state {
+		case htmlBase:
+			if c == '<' {
+				state = htmlTag
+				word = word[0:0]
+			}
+			_, err = bw.WriteRune(c)
+		case htmlTag:
+			if c == '>' {
+				if pattBody.MatchString(string(word)) {
+					state = htmlBody
+				} else {
+					state = htmlBase
+				}
+				word = word[0:0]
+			} else {
+				word = append(word, c)
+			}
+			_, err = bw.WriteRune(c)
+		case htmlBody:
+			if unicode.IsLetter(c) {
+				word = append(word, c)
+				break
+			}
+			if c == '<' {
+				state = htmlTagInBody
+			}
+			if len(word) > 0 {
+				parts := hyphenate.Word([]byte(string(word)), *h)
+				newword := bytes.Join(parts, utf8softhyphen)
+				_, err = bw.Write(newword)
+				if err != nil {
+					return err
+				}
+				word = word[0:0]
+			}
+			_, err = bw.WriteRune(c)
+		case htmlTagInBody:
+			if c == '>' {
+				state = htmlBody
+			}
+			_, err = bw.WriteRune(c)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	panic("not reached")
+}
 
 func hyph(epubpath, hyphpath string) error {
 	hh, err := os.Open(hyphpath)
@@ -22,22 +107,25 @@ func hyph(epubpath, hyphpath string) error {
 		return err
 	}
 
-	// Open a zip archive for reading.
+	// Open EPUB file as zip archive
 	r, err := zip.OpenReader(epubpath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Close()
 
-	// Iterate through the files in the archive,
-	// printing some of their contents.
 	for _, f := range r.File {
+		if filepath.Ext(f.Name) != ".html" {
+			continue
+		}
 		fmt.Printf("Contents of %s:\n", f.Name)
 		rc, err := f.Open()
 		if err != nil {
 			return err
 		}
-		_, err = io.CopyN(os.Stdout, rc, 68)
+
+		err = hyphHtml(rc, os.Stdout, hyph)
+		//_, err = io.CopyN(os.Stdout, rc, 68)
 		if err != nil && err != io.EOF {
 			return err
 		}
